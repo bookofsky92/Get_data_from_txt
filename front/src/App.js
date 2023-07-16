cb08fb3028b8aaf562e924404aa51a87c07eb95f
SerafimNikalayeu <serafim.nikalayeu@digitalinspirationlabs.io>
2023-07-17 02:53:24 +0300

    feat: end project

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import dropbox from 'dropbox';
+import uuid from 'react-uuid';
 
-const dropboxToken = 'sl.BiR5e9VDVpcDv4njcpm5iWSrtISIhpihwVdErvOBA0N579IsxFJV4bcCIdnR1iYCik8tU3nF1dJ5w96HL2dtt5crHXJ-HAmQ0g7jAE8aQ5cp03q5p1IldJ673O24cckYIc2ZpU4'; // Замените на ваш токен доступа Dropbox
+
+const dropboxToken = 'sl.BiXp4Aiut4urzSlsPsBXzQ1GmyQmRXP5RMq6gBVzFANKAzx5eRI6-k36jJ6kHQXtDPcIwpMCxuaCXojKT0V5LYx2vtEmTbnRnP_QOTgJpPbDo9Oa57yOLlfewD2AKU6YLJBnsT0'; // Замените на ваш токен доступа Dropbox
+
+
+function getNormalDate(dateS) {
+  const dateString = dateS.slice(0, -2);
+  const date = new Date(dateString);
+
+  const formattedDate = `${(date.getDate()).toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()}, ${(date.getHours()).toString().padStart(2, '0')}:${(date.getMinutes()).toString().padStart(2, '0')}:${(date.getSeconds()).toString().padStart(2, '0')}`;
+  return formattedDate;
+}
 
 const FileData = ({ fileName, fileData }) => {
+  console.log(fileName);
   const [latestEntry, setLatestEntry] = useState('');
 
   useEffect(() => {
@@ -23,28 +34,28 @@ const FileData = ({ fileName, fileData }) => {
   return (
     <div className="bg-white rounded-md shadow-md p-4 mb-4">
       <div className="text-center mb-4">
-        <h2 className="text-xl font-bold">{fileName}</h2>
+        <h2 className="text-xl font-bold">{fileName.split('.')[0]}</h2>
       </div>
       <div className="flex flex-col items-center">
         <div className="flex mb-2">
           <span className="font-semibold">Дата и время:</span>
-          <span className="ml-2">{date}</span>
+          <span className="ml-2">{getNormalDate(date)}</span>
         </div>
         <div className="flex mb-2">
           <span className="font-semibold">Показы:</span>
-          <span className="ml-2">{impressions}</span>
+          <span className="ml-2">{impressions && impressions.split(':')[1]}</span>
         </div>
         <div className="flex mb-2">
           <span className="font-semibold">Клики:</span>
-          <span className="ml-2">{clicks}</span>
+          <span className="ml-2">{clicks && clicks.split(':')[1]}</span>
         </div>
         <div className="flex mb-2">
           <span className="font-semibold">Конверсии:</span>
-          <span className="ml-2">{conversions}</span>
+          <span className="ml-2">{conversions && conversions.split(':')[1]}</span>
         </div>
         <div className="flex mb-2">
           <span className="font-semibold">Расход:</span>
-          <span className="ml-2">{spend}</span>
+          <span className="ml-2">{spend && spend.split(':')[1]}</span>
         </div>
       </div>
     </div>
@@ -56,15 +67,26 @@ const FooterTable = ({ fileName, fileData }) => {
 
   useEffect(() => {
     // Разбивка данных на отдельные строки
+    const allTablesData = [[]]
+    fileData = fileData.replace(/\s+\|\|/g, '||');
     const lines = fileData.split('\n');
     const rows = lines.map(line => line.split('||').map(entry => entry.trim()));
 
-    setTableData(rows);
+    rows.forEach((el, index) => {
+      const last = allTablesData.length - 1;
+      if (el[0] === '') {
+        allTablesData.push([]);
+      } else {
+        allTablesData[last].push(el);
+      }
+    })
+
+    setTableData(allTablesData);
   }, [fileData]);
 
   return (
     <div className="bg-white rounded-md shadow-md p-4 mb-4">
-      <h2 className="text-xl font-bold mb-4">{fileName}</h2>
+      <h2 className="text-xl font-bold mb-4">{fileName.split('.')[0]}</h2>
       <table className="w-full border-collapse">
         <thead>
           <tr>
@@ -81,10 +103,15 @@ const FooterTable = ({ fileName, fileData }) => {
         </thead>
         <tbody>
           {tableData.map((row, index) => (
-            <tr key={index}>
-              {row.map((entry, idx) => (
-                <td key={idx} className="p-2 border">{entry}</td>
-              ))}
+            <tr>
+              {
+                row.map((el, index) => (
+                  (el && el[0] && el[0] !== '') ?
+                  <td key={uuid()} className="p-2 border">{el[0].includes('(') ? getNormalDate(el[0]) : el[0]}</td>
+                  :
+                  ''
+                ))
+              }
             </tr>
           ))}
         </tbody>
@@ -97,30 +124,55 @@ const App = () => {
   const [fileData, setFileData] = useState([]);
 
   useEffect(() => {
-    // Инициализация Dropbox
-    const dbx = new dropbox.Dropbox({ accessToken: dropboxToken });
-
-    // Получение списка файлов
-    dbx.filesListFolder({ path: '/' })
-      .then(response => {
-        const files = response.entries;
-        const promises = files.map(file => {
-          // Загрузка содержимого каждого файла
-          return dbx.filesDownload({ path: file.path_display })
-            .then(fileData => {
-              const data = fileData.fileBinary.toString();
-              setFileData(prevData => [...prevData, { fileName: file.name, fileData: data }]);
-            });
+    const fetchFilesFromDropbox = async () => {
+      try {
+        const response = await fetch('https://api.dropboxapi.com/2/files/list_folder', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${dropboxToken}`,
+          },
+          body: JSON.stringify({
+            path: '/tests',
+          }),
         });
 
-        Promise.all(promises)
-          .catch(error => {
-            console.error('Ошибка при загрузке файлов из Dropbox:', error);
+        if (response.ok) {
+          const data = await response.json();
+          const files = data.entries;
+
+          const promises = files.map(async (file) => {
+            const fileResponse = await fetch('https://content.dropboxapi.com/2/files/download', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/octet-stream',
+                Authorization: `Bearer ${dropboxToken}`,
+                'Dropbox-API-Arg': JSON.stringify({
+                  path: file.path_display,
+                }),
+              },
+            });
+
+            if (fileResponse.ok) {
+              const fileData = await fileResponse.text();
+              return { fileName: file.name, fileData };
+            } else {
+              console.error('Ошибка при загрузке файла из Dropbox:', file.name);
+              return null;
+            }
           });
-      })
-      .catch(error => {
-        console.error('Ошибка при получении списка файлов из Dropbox:', error);
-      });
+
+          const fileData = await Promise.all(promises);
+          setFileData(fileData.filter((file) => file !== null));
+        } else {
+          console.error('Ошибка при получении списка файлов из Dropbox:', response.status);
+        }
+      } catch (error) {
+        console.error('Ошибка при выполнении запроса к Dropbox API:', error);
+      }
+    };
+
+    fetchFilesFromDropbox();
   }, []);
 
   return (
@@ -128,7 +180,7 @@ const App = () => {
       <h1 className="text-3xl font-bold text-center mb-8">Данные файлов</h1>
       {fileData.map(({ fileName, fileData }) => (
         <React.Fragment key={fileName}>
-          {fileName.includes('summary') ? (
+          {fileName.includes('statistic_pb') ? (
             <FooterTable fileName={fileName} fileData={fileData} />
           ) : (
             <FileData fileName={fileName} fileData={fileData} />
