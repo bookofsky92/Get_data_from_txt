42d3638e0af26918e0538c08b68a60503f653317
SerafimNikalayeu <serafim.nikalayeu@digitalinspirationlabs.io>
2023-07-17 15:56:05 +0300

    fix: logs

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import uuid from 'react-uuid';
+import axios from 'axios';
 
 
-const dropboxToken = 'sl.BiU7yMIwdgY6DdiHU8lcRHpIvq5FFwdFitj4ivGFjT4NH9y7JRWWSAgu58eHwLsJf02Ljdq5Q0oTFnXTUojp6P4tytPhTXNiSkyqA19bhNveGazDU16Fs5kfIht6ujEP6ceeJP8'; // Замените на ваш токен доступа Dropbox
-
+// const dropboxToken = 'sl.BiU7yMIwdgY6DdiHU8lcRHpIvq5FFwdFitj4ivGFjT4NH9y7JRWWSAgu58eHwLsJf02Ljdq5Q0oTFnXTUojp6P4tytPhTXNiSkyqA19bhNveGazDU16Fs5kfIht6ujEP6ceeJP8'; // Замените на ваш токен доступа Dropbox
+// const dropboxToken = 'sl.BiWLfNuBKvpF70ytGjrP0rsGr1gZ_rrP7TfMfm70FaPsKxM1kzyx8kd5W9j-uK2yv5ZmhKhBZc6UTqKyDaSPBFLGj0FRpVgb2otd80XNTjzyOE6ZEbFOADoW7IZld_9h3cmMOZ0'
 
 function getNormalDate(dateS) {
   const dateString = dateS.slice(0, -2);
@@ -122,64 +123,107 @@ const FooterTable = ({ fileName, fileData }) => {
 const App = () => {
   const [fileData, setFileData] = useState([]);
   const [error, setError] = useState("");
+  const [dropboxToken, setDropboxToken] = useState(""); // Состояние для токена Dropbox
+  const [isLoggedIn, setIsLoggedIn] = useState(false); // Состояние для проверки входа пользователя
+
 
   useEffect(() => {
     const fetchFilesFromDropbox = async () => {
       try {
-        const response = await fetch('https://api.dropboxapi.com/2/files/list_folder', {
-          method: 'POST',
+        const currentToken = localStorage.getItem('token');
+        if (!currentToken) {
+          return; // Прекратить выполнение, если токен отсутствует
+        } else {
+          setDropboxToken(currentToken);
+        }
+
+        
+
+    // Запрос списка файлов из Dropbox с использованием токена
+    const response = await axios.post('https://api.dropboxapi.com/2/files/list_folder', {
+      path: '/tests',
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${dropboxToken}`,
+      },
+    });
+
+    if (response.status === 200) {
+      const files = response.data.entries;
+      const promises = files.map(async (file) => {
+        const fileResponse = await axios.post('https://content.dropboxapi.com/2/files/download', null, {
           headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${dropboxToken}`,
+            'Content-Type': 'application/octet-stream',
+            'Authorization': `Bearer ${dropboxToken}`,
+            'Dropbox-API-Arg': JSON.stringify({
+              path: file.path_display,
+            }),
           },
-          body: JSON.stringify({
-            path: '/tests',
-          }),
         });
 
-        if (response.ok) {
-          const data = await response.json();
-          const files = data.entries;
-
-          const promises = files.map(async (file) => {
-            const fileResponse = await fetch('https://content.dropboxapi.com/2/files/download', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/octet-stream',
-                Authorization: `Bearer ${dropboxToken}`,
-                'Dropbox-API-Arg': JSON.stringify({
-                  path: file.path_display,
-                }),
-              },
-            });
-
-            if (fileResponse.ok) {
-              const fileData = await fileResponse.text();
-              return { fileName: file.name, fileData };
-            } else {
-              console.error('Ошибка при загрузке файла из Dropbox:', file.name);
-              return null;
-            }
-          });
-
-          const fileData = await Promise.all(promises);
-          setFileData(fileData.filter((file) => file !== null));
+        if (fileResponse.status === 200) {
+          const fileData = fileResponse.data;
+          return { fileName: file.name, fileData };
         } else {
-          console.error('Ошибка при получении списка файлов из Dropbox:', response.status);
-          setError("Обновите api токен для DropBox");
+          console.error('Ошибка при загрузке файла из Dropbox:', file.name);
+          return null;
         }
-      } catch (error) {
-        console.error('Ошибка при выполнении запроса к Dropbox API:', error);
-      }
+      });
+
+      const fileData = await Promise.all(promises);
+      setFileData(fileData.filter((file) => file !== null));
+    } else {
+      console.error('Ошибка при получении списка файлов из Dropbox:', response.status);
+      setError("Обновите api токен для Dropbox");
+    }
+  } catch (error) {
+    console.error('Ошибка при выполнении запроса к Dropbox API:', error);
+  }
     };
 
     fetchFilesFromDropbox();
+  }, [dropboxToken]);
+
+  const handleLogin = () => {
+    // Перенаправление на страницу аутентификации Dropbox OAuth
+    window.location.href = 'https://www.dropbox.com/oauth2/authorize?client_id=zibwpo89o249746&response_type=token&redirect_uri=http://localhost:3000';
+  };
+
+  const handleOAuthCallback = () => {
+    const token = window.location.hash.split('=')[1]; // Получение токена из URL
+    const cleanToken = token.split('&')[0];
+    localStorage.setItem('token', cleanToken);
+    setDropboxToken(cleanToken); // Установка токена Dropbox
+    setIsLoggedIn(true); // Установка состояния входа пользователя
+
+    // Очистка URL от токена
+    window.history.replaceState({}, document.title, window.location.pathname);
+  };
+
+  useEffect(() => {
+    // Проверка, был ли выполнен вход при загрузке страницы
+    if (window.location.hash.includes('access_token')) {
+      handleOAuthCallback();
+    }
   }, []);
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold text-center mb-8">Данные файлов</h1>
-      {error != "" && <h1 className='text-3xl font-bold text-center mt-4 text-red-600'>error</h1>}
+
+      {!isLoggedIn ? (
+        <div className="flex justify-center mb-4">
+          <button className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md" onClick={handleLogin}>
+            Войти в аккаунт Dropbox
+          </button>
+        </div>
+      ) : ''}
+
+      {error !== "" && (
+        <h1 className="text-3xl font-bold text-center mt-4 text-red-600">{error}</h1>
+      )}
+
       {fileData.map(({ fileName, fileData }) => (
         <React.Fragment key={fileName}>
           {!fileName.includes('statistic_pb') && (
@@ -187,6 +231,7 @@ const App = () => {
           )}
         </React.Fragment>
       ))}
+
       {fileData.map(({ fileName, fileData }) => (
         <React.Fragment key={fileName}>
           {fileName.includes('statistic_pb') && (
@@ -194,9 +239,6 @@ const App = () => {
           )}
         </React.Fragment>
       ))}
-
-      
-
     </div>
   );
 };
