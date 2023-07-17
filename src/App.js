2eebb76bfc346d501ce49a08bf23f3f815062b01
Александр Дьячин <37014076+bit-x@users.noreply.github.com>
2023-07-17 23:17:37 +0300

    change path to ''

--- src/App.js
+++ src/App.js
@@ -143,7 +143,7 @@ const App = () => {
 
         // Запрос списка файлов из Dropbox с использованием токена
         const response = await axios.post('https://api.dropboxapi.com/2/files/list_folder', {
-          path: '/',
+          path: '',
         }, {
           headers: {
             'Content-Type': 'application/json',
