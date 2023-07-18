69173006ca4cf32ccbac3ac63e208f586466562e
Александр Дьячин <37014076+bit-x@users.noreply.github.com>
2023-07-19 00:33:43 +0300

    change time type

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function getNormalDate(dateS) {
   const dateString = dateS.slice(0, -2);
   const date = new Date(dateString);
 
-  const formattedDate = `${(date.getDate()).toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()}, ${(date.getHours()).toString().padStart(2, '0')}:${(date.getMinutes()).toString().padStart(2, '0')}:${(date.getSeconds()).toString().padStart(2, '0')}`;
+  const formattedDate = `${(date.getDate()).toString().padStart(2, '0')}.${(date.getMonth() + 1).toString().padStart(2, '0')}.${date.getFullYear()}, ${(date.getHours() === 0 ? 1 : date.getHours() + 1).toString().padStart(2, '0')}:${(date.getMinutes()).toString().padStart(2, '0')}:${(date.getSeconds()).toString().padStart(2, '0')}`;
   return formattedDate;
 }
 
