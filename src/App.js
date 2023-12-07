ef097ecf996078e510018dc36770b7d66b0670ca
SerafimNikalayeu <serafim.nikalayeu@digitalinspirationlabs.io>
2023-12-07 22:47:33 +0300

    feat: spending + 20%, balance field, 5 in row

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ const FileData = ({ fileName, fileData }) => {
   const entries = latestEntry.split('||').map(entry => entry.trim());
 
   // Получение значений из последней строки
-  const [date, impressions, clicks, conversions, spend] = entries;
+  const [date, impressions, clicks, conversions, spend, balance] = entries;
 
   return (
     <div className="bg-white rounded-md shadow-md p-4 mb-4 w-80">
@@ -42,6 +42,10 @@ const FileData = ({ fileName, fileData }) => {
         <div className="flex mb-2">
           <span className="font-semibold">Дата и время:</span>
           <span className="ml-2">{getNormalDate(date)}</span>
+        </div>
+        <div className="flex mb-2">
+          <span className="font-semibold">Баланс:</span>
+          <span className="ml-2">{balance && balance.split(':')[1]}</span>
         </div>
         <div className="flex mb-2">
           <span className="font-semibold">Показы:</span>
@@ -57,7 +61,7 @@ const FileData = ({ fileName, fileData }) => {
         </div>
         <div className="flex mb-2">
           <span className="font-semibold">Расход:</span>
-          <span className="ml-2">{spend && spend.split(':')[1]}</span>
+          <span className="ml-2">{spend && (parseFloat(spend.split(':')[1].replace(/[^\d,]/g, '').replace(',', '.')) * 1.2).toFixed(2)}</span>
         </div>
       </div>
     </div>
@@ -74,19 +78,32 @@ const AllStatisticTable = ({filesData}) => {
         const lines = fileData.split('\n');
         const lastLine = lines[lines.length - 2]; // Игнорируем последнюю пустую строку
         const entries = lastLine.split('||').map(entry => entry.trim());
-        const [date, impressions, clicks, conversions, spend] = entries;
-        const spendings = spend.split(':')[1];
+        const [date, impressions, clicks, conversions, spend, balance] = entries;
+        const spendings = parseFloat(spend.split(':')[1].replace(/[^\d,]/g, '').replace(',', '.')) * 1.2; // adding 20% to spendings
         const impr = impressions.split(':')[1];
+
+        console.log('clicks - ', clicks)
+
         if (tableDataArray.length === 0) {
-          tableDataArray = [+impr.replace(/\s/g, ""), +clicks.split(':')[1], +conversions.split(':')[1], parseFloat(spendings.replace(',', '.'))];
+          tableDataArray = [ parseFloat(balance.replace(/[^\d,]/g, '').replace(',', '.')), +impr.replace(/\s/g, ""), +clicks.split(':')[1], +conversions.split(':')[1], spendings];
         } else {
-          tableDataArray[0] += +impr.replace(/\s/g, "");
-          tableDataArray[1] += +clicks.split(':')[1];
-          tableDataArray[2] += +conversions.split(':')[1];
-          tableDataArray[3] += parseFloat(spendings.replace(',', '.'));
+          tableDataArray[0] += parseFloat(balance.replace(/[^\d,]/g, '').replace(',', '.'));
+          tableDataArray[1] += +impr.replace(/\s/g, "");
+          tableDataArray[2] += +clicks.split(':')[1].replace(/[^\d,]/g, '');
+          tableDataArray[3] += +conversions.split(':')[1];
+          tableDataArray[4] += spendings;
         }
       }
     })
+
+    if (tableDataArray[0]) {
+      tableDataArray[0] = tableDataArray[0].toFixed(2) + ' руб.';
+    }
+
+    if (tableDataArray[4]) {
+      tableDataArray[4] = tableDataArray[4].toFixed(2) + ' руб.';
+    }
+
     setTableData(tableDataArray);
   }, [tableData]);
 
@@ -97,6 +114,7 @@ const AllStatisticTable = ({filesData}) => {
       <table className="w-full table-auto border-collapse">
         <thead>
           <tr>
+            <th className="p-2 border">Баланс</th>
             <th className="p-2 border">Показы</th>
             <th className="p-2 border">Клики</th>
             <th className="p-2 border">Конверсии</th>
@@ -247,7 +265,7 @@ const App = () => {
 
   const handleLogin = () => {
     // Перенаправление на страницу аутентификации Dropbox OAuth
-    window.location.href = 'https://www.dropbox.com/oauth2/authorize?client_id=gugnlzdos7meaiq&response_type=token&redirect_uri=https://getting-data-from-txt.vercel.app'; // Вот тут после redirect_uri= напишите ваш сайт куда вы залили этот проект ||| то есть вам надо заменить https://get-data-from-txt.vercel.app на свою ссылку
+    window.location.href = 'https://www.dropbox.com/oauth2/authorize?client_id=gugnlzdos7meaiq&response_type=token&redirect_uri=http://localhost:3000/'; // --- https://getting-data-from-txt.vercel.app --- Вот тут после redirect_uri= напишите ваш сайт куда вы залили этот проект ||| то есть вам надо заменить https://get-data-from-txt.vercel.app на свою ссылку
   };
 
   const handleOAuthCallback = () => {
@@ -269,7 +287,7 @@ const App = () => {
   }, []);
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 max-w-[1750px]">
       <h1 className="text-3xl font-bold text-center mb-8">Статистика</h1>
 
       {!isLoggedIn ? (
@@ -283,14 +301,15 @@ const App = () => {
       {error !== "" && (
         <h1 className="text-3xl font-bold text-center mt-4 text-red-600">{error}</h1>
       )}
-      <div className="flex flex-wrap just justify-center gap-4">
-        {fileData.map(({ fileName, fileData }) => (
-          <React.Fragment key={fileName}>
-            {!fileName.includes('statistic_pb') && (
-              <FileData fileName={fileName} fileData={fileData} />
-            )}
-          </React.Fragment>
-        ))}
+      <div className="flex flex-wrap justify-center gap-4">
+        {fileData
+            .filter(({ fileName }) => !fileName.includes('statistic_pb'))
+            .sort((a, b) => a.fileName.localeCompare(b.fileName))
+            .map(({ fileName, fileData }) => (
+                <React.Fragment key={fileName}>
+                  <FileData fileName={fileName} fileData={fileData} />
+                </React.Fragment>
+            ))}
       </div>
       <React.Fragment>
           <AllStatisticTable filesData={fileData} />
